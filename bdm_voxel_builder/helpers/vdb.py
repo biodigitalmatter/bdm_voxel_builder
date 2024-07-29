fa320135a61bd4e23c568739007b417c0e5508a6
Anton Tetov <anton@tetov.se>
2024-07-29 16:41:37 +0200

    get xform from vdb files

---
+++ bdm_voxel_builder/helpers/vdb.py
@@ -0,0 +1,28 @@
+import numpy as np
+import compas.geometry as cg
+import pyopenvdb as vdb
+
+
+def _get_vdb_matrix(vdb_xform: vdb.Transform) -> list[list[float]]:
+    """Extracts the matrix from a vdb.Transform object.
+
+    Haven't figured out a better way to do this yet.
+    """
+    text = vdb_xform.info().strip().split("\n")[2:]
+    M = []
+    for line in text:
+        str_parts = (
+            line.replace("[", "").replace("]", "").replace(",", "").strip().split()
+        )
+
+        float_parts = [float(s) for s in str_parts]
+        M.append(float_parts)
+    return np.array(M).transpose().tolist()
+
+
+def xform_to_vdb(xform: cg.Transformation) -> vdb.Transform:
+    return vdb.createLinearTransform(np.array(xform.matrix).transpose())
+
+
+def xform_to_compas(xform: vdb.Transform) -> cg.Transformation:
+    return cg.Transformation.from_matrix(_get_vdb_matrix(xform))
---
+++ tests/helpers/test_vbd.py
@@ -0,0 +1,69 @@
+import numpy as np
+import compas.geometry as cg
+import pyopenvdb as vdb
+import pytest
+from bdm_voxel_builder.helpers.vdb import _get_vdb_matrix, xform_to_compas, xform_to_vdb
+
+
+@pytest.fixture
+def identity_xform_vdb():
+    return vdb.createLinearTransform()
+
+
+@pytest.fixture
+def identity_xform_compas():
+    return cg.Transformation()
+
+
+@pytest.fixture
+def other_xform_matrix():
+    return [[1, 0, 0, 10], [0, 1, 0, 20], [0, 0, 1, 30], [0, 0, 0, 1]]
+
+
+@pytest.fixture
+def other_xform_vdb(other_xform_matrix):
+    return vdb.createLinearTransform(np.array(other_xform_matrix).transpose().tolist())
+
+
+@pytest.fixture
+def other_xform_compas(other_xform_matrix):
+    return cg.Transformation.from_matrix(other_xform_matrix)
+
+
+def assert_equal_vdb_compas_xforms(
+    vdb_xform: vdb.Transform, compas_xform: cg.Transformation
+):
+    assert vdb_xform.isLinear
+    vdb_matrix = _get_vdb_matrix(vdb_xform)
+    assert vdb_matrix == compas_xform.matrix
+
+
+def test__get_vdb_matrix(
+    identity_xform_vdb, identity_xform_compas, other_xform_vdb, other_xform_compas
+):
+    assert_equal_vdb_compas_xforms(
+        vdb_xform=identity_xform_vdb, compas_xform=identity_xform_compas
+    )
+
+    assert_equal_vdb_compas_xforms(
+        vdb_xform=other_xform_vdb,
+        compas_xform=other_xform_compas,
+    )
+
+
+def test_xform_to_vdb(identity_xform_compas, other_xform_compas):
+    vdb_xform = xform_to_vdb(identity_xform_compas)
+    assert_equal_vdb_compas_xforms(vdb_xform, identity_xform_compas)
+
+    vdb_xform = xform_to_vdb(other_xform_compas)
+    assert_equal_vdb_compas_xforms(vdb_xform, other_xform_compas)
+
+
+def test_xform_to_compas(identity_xform_vdb, other_xform_vdb):
+    compas_xform = xform_to_compas(identity_xform_vdb)
+    assert_equal_vdb_compas_xforms(
+        compas_xform=compas_xform, vdb_xform=identity_xform_vdb
+    )
+
+    compas_xform = xform_to_compas(other_xform_vdb)
+    assert_equal_vdb_compas_xforms(compas_xform=compas_xform, vdb_xform=other_xform_vdb)
