7806a4437641b207f466f1f8d720b60c6edd1c87
Anton Tetov <anton@tetov.se>
2024-07-30 18:28:11 +0200

    fixes and tests for datalayer

--- tests/data_layers/test_base.py
+++ tests/data_layers/test_base.py
@@ -1,3 +1,4 @@
+import compas.geometry as cg
 import numpy as np
 import pytest
 
@@ -24,3 +25,28 @@ def test_save_vdb(random_int_array):
     path = layer.save_vdb()
 
     assert path.exists()
+
+
+def test_get_local_bbox():
+    layer = DataLayer(grid_size=(10, 10, 10))
+    bbox = layer.get_local_bbox()
+    assert bbox.diagonal == ((0, 0, 0), (10, 10, 10))
+
+
+def test_get_local_bbox_with_non_square_grid_size():
+    with pytest.raises(NotImplementedError):
+        layer = DataLayer(grid_size=(5, 10, 15))
+        layer.get_local_bbox()
+
+
+def test_get_world_bbox():
+    layer = DataLayer(grid_size=(10, 10, 10))
+    bbox = layer.get_world_bbox()
+    assert bbox.diagonal == ((0, 0, 0), (10, 10, 10))
+
+
+def test_get_world_bbox_with_non_square_grid_size():
+    T = cg.Translation.from_vector([15, 10, 5])
+    layer = DataLayer(grid_size=(5, 5, 5), xform=T)
+    bbox = layer.get_world_bbox()
+    assert bbox.diagonal == cg.Line((15, 10, 5), (20, 15, 10))
