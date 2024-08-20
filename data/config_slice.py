2531af711e46e2d852d04c1a99457bb7b993eaa2
railgnam <57008999+railgnam@users.noreply.github.com>
2024-08-20 12:17:11 +0200

    create algo 10 c

--- data/config_slice.py
+++ data/config_slice.py
@@ -3,20 +3,23 @@
 from bdm_voxel_builder.agent_algorithms.algo_10_b_slicer_agents import (
     Algo10b_VoxelSlicer,
 )
+from bdm_voxel_builder.agent_algorithms.algo_10_c_slicer_agents_import import (
+    Algo10c_VoxelSlicer,
+)
 from bdm_voxel_builder.config_setup import Config
 from bdm_voxel_builder.visualizer.compas_viewer import CompasViewerVisualizer
 
 # test slice
 grid_size = 30
-iterations = 1000
+iterations = 10
 agent_count = 1
-interval = 1000
+interval = 10
 name = f"test_config_slice_b_i{iterations}a{agent_count}"
 
 config = Config(
     iterations=iterations,
     grid_size=grid_size,
-    algo=Algo10b_VoxelSlicer(
+    algo=Algo10c_VoxelSlicer(
         agent_count=agent_count,
         grid_size=grid_size,
         name=name,
