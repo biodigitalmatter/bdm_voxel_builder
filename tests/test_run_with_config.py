5b080b48a839e5f8d418dc505714a37ffd460bc0
Anton Tetov <anton@tetov.se>
2024-11-04 17:32:35 +0100

    monkeypatch compas viewer

--- tests/test_run_with_config.py
+++ tests/test_run_with_config.py
@@ -1,11 +1,18 @@
+from unittest.mock import patch
+
 from bdm_voxel_builder import DATA_DIR
 from bdm_voxel_builder.__main__ import _load_config, run_algo
 from bdm_voxel_builder.config_setup import Config
 
 
-def test_run_with_config():
+@patch(
+    "bdm_voxel_builder.visualizer.compas_viewer.CompasViewerVisualizer.__init__",
+    return_value=None,
+)
+def test_run_with_config(mock_viewer):
     config: Config = _load_config(DATA_DIR / "config.py")
     config.iterations = 10
     config.visualizer = None
 
     run_algo(config)
+    mock_viewer.assert_called_once()
